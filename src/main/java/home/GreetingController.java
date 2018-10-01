44e4a6cb8144f4cb892ca97708f6268c115ea23f
Admin <borodinmp@mail.ru>
2018-10-01 16:12:24 +0700

    Second Commit

--- src/main/java/home/GreetingController.java
+++ src/main/java/home/GreetingController.java
@@ -1,17 +1,21 @@
 package home;
 
+import home.domain.Message;
+import home.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.annotation.*;
+import java.util.List;
 import java.util.Map;
 
 @Controller
     public class GreetingController {
+
+    @Autowired
+    private MessageRepo messageRepo;
 
         @GetMapping("/greeting")
         public String greeting(
@@ -23,7 +27,31 @@ import java.util.Map;
 
         @GetMapping
         public String main(Map<String,Object> model) {
-            model.put("Hello", "Lets code!");
+            Iterable<Message> messages = messageRepo.findAll();
+            model.put("messages", messages);
+            return "main";
+        }
+
+        @PostMapping
+        public String add(@RequestParam String text, @RequestParam String tag, Map<String,Object> model) {
+        Message message = new Message(text,tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+
+            return "main";
+        }
+
+        @PostMapping("filter")
+        public String filter(@RequestParam String filter, Map<String,Object> model) {
+            Iterable<Message> messages;
+            if(filter != null && !filter.isEmpty()) {
+                messages = messageRepo.findByTag(filter);
+            } else {
+                messages = messageRepo.findAll();
+            }
+            model.put("messages", messages);
             return "main";
         }
 
