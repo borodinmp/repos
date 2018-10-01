0b91abe32c4a5c7a61e372fd53628e8daa143c76
Admin <borodinmp@mail.ru>
2018-10-01 08:46:43 +0700

    First Commit

---
+++ src/main/java/home/GreetingController.java
@@ -0,0 +1,36 @@
+package home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.lang.annotation.*;
+import java.util.Map;
+
+@Controller
+    public class GreetingController {
+
+        @GetMapping("/greeting")
+        public String greeting(
+                @RequestParam(name="name", required=false, defaultValue="World") String name,
+                Map<String,Object> model) {
+            model.put("name", name);
+            return "greeting";
+        }
+
+        @GetMapping
+        public String main(Map<String,Object> model) {
+            model.put("Hello", "Lets code!");
+            return "main";
+        }
+
+
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface GetMapping {
+    }
+    }
+
