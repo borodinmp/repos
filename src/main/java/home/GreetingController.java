87470b7cf00fc41794c01092139bda9be64c2a4c
Admin <borodinmp@mail.ru>
2018-10-03 08:34:04 +0700

    Commit - 4

--- src/main/java/home/GreetingController.java
+++ src/main/java/home/GreetingController.java
@@ -3,46 +3,40 @@ package home;
 import home.domain.Message;
 import home.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.soap.Node;
-import java.lang.annotation.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
-@Controller
+    @Controller
     public class GreetingController {
 
-    @Autowired
-    private MessageRepo messageRepo;
+        @Autowired
+        private MessageRepo messageRepo;
 
-        @GetMapping("/greeting")
-    public String greeting(
+        @GetMapping("/")
+        public String greeting(
             @RequestParam(name="name", required=false, defaultValue="World") String name,
             Map<String,Object> model) {
-        model.put("name", name);
-        return "greeting";
-    }
+            model.put("name", name);
+            return "greeting.mustache";
+            }
 
-    @GetMapping
-    public String main(Map<String,Object> model) {
-        Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
-        return "main";
-    }
+        @GetMapping("/main")
+        public String main(Map<String,Object> model) {
+            Iterable<Message> messages = messageRepo.findAll();
+            model.put("messages", messages);
+            return "main";
+            }
 
-        @PostMapping
+        @PostMapping("/main")
         public String add(@RequestParam String nameOrg, @RequestParam String inn, @RequestParam String ogrnn,
                           @RequestParam String addressOrg, Map<String,Object> model) {
-        Message message = new Message(nameOrg, inn, ogrnn, addressOrg);
-        messageRepo.save(message);
+            Message message = new Message(nameOrg, inn, ogrnn, addressOrg);
+            messageRepo.save(message);
 
-        Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
+            Iterable<Message> messages = messageRepo.findAll();
+            model.put("messages", messages);
             return "main";
         }
 
@@ -58,17 +52,17 @@ import java.util.Map;
             }
             model.put("messages", messages);
             return "main";
-        }
+            }
 
-    @DeleteMapping("delete")
-    public String delete(@RequestParam Integer id, Map<String,Object> model) {
+        @DeleteMapping("delete")
+        public String delete(@RequestParam Integer id, Map<String,Object> model) {
 
-        Iterable<Message> messages;
+            Iterable<Message> messages;
 
             messages = messageRepo.deleteById(id);
             model.remove("messages", messages);
             return "main";
-        }
+            }
 
     }
 
